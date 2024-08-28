bfcf06d1e42bb7b969fad78bd3e97dd1c6ad1740
WooSungChoi99 <open.w.sung@gmail.com>
2024-08-28 18:30:01 +0900

    Title: 함수를 이용한 369 게임, Time: 167ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240828/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240828/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,22 +1,23 @@
-# a, b = tuple(map(int, input().split()))
+a, b = tuple(map(int, input().split()))
 
-# def count_magic_num(a, b) :
-#     cnt = 0
-#     for num in range(a, b+1) :
-#         if num%3==0 or is_magic_number(num) :
-#             cnt += 1
-#             #print(num)
-#     return cnt
+def count_magic_num(a, b) :
+    cnt = 0
+    for num in range(a, b+1) :
+        if num%3==0 or is_magic_number(num) :
+            cnt += 1
+            #print(num)
+    return cnt
 
-# def is_magic_number(num) :
-#     if num%10==3 or num%10==6 or num%10==9 :
-#         return True
-#     for i in range(1, len(str(num))) :
-#         if (num//10**i)==3 or (num//10**i)==6 or (num//10**i)==9 :
-#             #print(num)
-#             return True
+def is_magic_number(num) :
+    if num%10==3 or num%10==6 or num%10==9 :
+        return True
+    for i in range(1, len(str(num))) :
+        if (num//10**i)==3 or (num//10**i)==6 or (num//10**i)==9 :
+            #print(num)
+            return True
+    return False
 
-# print(count_magic_num(a, b))
+print(count_magic_num(a, b))
 
 
 
@@ -29,26 +30,26 @@
 # #3. count를 출력해준다.
 
 
-a, b = tuple(map(int, input().split()))
+# a, b = tuple(map(int, input().split()))
 
-#조건 2
-def mult_three(n) :
-    return n % 3 == 0
+# #조건 2
+# def mult_three(n) :
+#     return n % 3 == 0
 
-def thr_six_nine(n) :
-    while n > 0 :
-        if n%10 == 3 or n%10 == 6 or n%10 == 9 :
-            return True
-        n = n // 10
-    return False
-
-
-def correct_num(a, b) :
-    count = 0
-    for i in range (a, b+1) :
-        if mult_three(i) or thr_six_nine(i) :
-            count += 1
-    return count
+# def thr_six_nine(n) :
+#     while n > 0 :
+#         if n%10 == 3 or n%10 == 6 or n%10 == 9 :
+#             return True
+#         n = n // 10
+#     return False
 
 
-print(correct_num(a, b))
+# def correct_num(a, b) :
+#     count = 0
+#     for i in range (a, b+1) :
+#         if mult_three(i) or thr_six_nine(i) :
+#             count += 1
+#     return count
+
+
+# print(correct_num(a, b))
