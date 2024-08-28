13df912ef6fa89874058d63ca3cd53956bfcf8d7
WooSungChoi99 <open.w.sung@gmail.com>
2024-08-28 17:11:35 +0900

    Title: 최소공배수 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240828/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,26 @@
+def min_mult_num(n, m) :
+    min_mult_num = n*m
+    for num in range(1, max(n, m)) :
+        if (min(n, m)*num)%max(n, m)==0 :
+            min_mult_num = min(n, m)*num
+            return min_mult_num
+    return min_mult_num
+
+n, m = tuple(map(int, input().split()))
+
+print(min_mult_num(n, m))
+        
+
+
+
+# n, m = tuple(map(int, input().split()))
+
+# def max_corr(n, m) :
+#     for num in range (max(n, m), (n*m)+1) :
+#         if num % min(n, m) == 0 and num % max(n, m) == 0 :
+#             max_corr_num = num
+#             break
+
+#     print(max_corr_num)
+
+# max_corr(n, m)
