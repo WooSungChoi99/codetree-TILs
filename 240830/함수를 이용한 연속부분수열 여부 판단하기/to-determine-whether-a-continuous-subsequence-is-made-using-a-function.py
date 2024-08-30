633a8063bd6d93e283e4cd512468d8029b8c7b94
WooSungChoi99 <open.w.sung@gmail.com>
2024-08-30 20:12:27 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240830/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,41 @@
+n1, n2 = tuple(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+def is_partial(n1, A, n2, B) :
+    for i in range(n1-n2+1) :
+        if A[i:i+n2] == B :
+            return True
+    return False
+
+if is_partial(n1, A, n2,  B) :
+    print("Yes")
+else :
+    print("No")
+
+
+
+
+
+
+
+
+
+
+# a, b = tuple(map(int, input().split()))
+# list_a = list(map(int, input().split()))
+# list_b = list(map(int, input().split()))
+
+
+# def continuous_partial_sequence(a, b, list_a, list_b) :
+#     for i in range(a) :
+#         if list_b[0] == list_a[i] :
+#             if list_b == list_a[i:i+b] :
+#                 return True
+#     return False
+
+
+# if continuous_partial_sequence(a, b, list_a, list_b) :
+#     print("Yes")
+# else :
+#     print("No")
