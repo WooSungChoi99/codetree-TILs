b1b76e7faf57e4f6e9d1637b8103769d41c0183c
WooSungChoi99 <open.w.sung@gmail.com>
2024-09-02 19:32:51 +0900

    Title: 두 정수에 대한 연산값, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240902/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,13 @@
+a, b = tuple(map(int, input().split()))
+
+def follow_order(a, b) :
+    if a > b :
+        a += 25
+        b *= 25
+    else :
+        a *= 2
+        b += 25
+    
+    return a, b
+
+print(follow_order(a, b)[0], follow_order(a, b)[1])
