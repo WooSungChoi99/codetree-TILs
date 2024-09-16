0e0a30650126fb8e3a23a48540cce2c4af7cf8fb
WooSungChoi99 <open.w.sung@gmail.com>
2024-09-16 19:26:14 +0900

    Title: 선택 정렬 구현, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240916/선택 정렬 구현/implement-selection-sort.py
+++ 240916/선택 정렬 구현/implement-selection-sort.py
@@ -5,9 +5,8 @@ for i in range(n-1):
     min = i
     for j in range(i+1, n):
         if arr[j]<arr[min]:
-            temp = arr[min]
-            arr[min] = arr[j]
-            arr[j] = temp
-            
+            min = j
+    arr[i], arr[min] = arr[min], arr[i]
+
 for elem in arr:
     print(elem, end=" ")
