9b886299ddabe8228576d29a9256391ddb9ed449
WooSungChoi99 <open.w.sung@gmail.com>
2024-12-14 23:48:33 +0900

    Title: 정수 명령 처리 8, Time: 82ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241214/정수 명령 처리 8/process-numeric-commands-8.py
@@ -0,0 +1,278 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+
+# class DoublyLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#         self.node_num = 0
+
+#     def push_front(self, new_data):
+#         new_node = Node(new_data)
+#         new_node.next = self.head
+
+#         if self.head != None:
+#             self.head.prev = new_node
+#             self.head = new_node
+#             new_node.prev = None
+
+#         else:
+#             self.head = new_node
+#             self.tail = new_node
+#             self.prev = None
+
+#         self.node_num += 1
+    
+#     def push_back(self, new_data):
+#         new_node = Node(new_data)
+#         new_node.prev = self.tail
+
+#         if self.tail != None :
+#             self.tail.next = new_node
+#             self.tail = new_node
+#             new_node.next = None
+
+#         else :
+#             self.head = new_node
+#             self.tail = new_node
+#             new_node.next = None
+
+#         self.node_num += 1
+
+#     def pop_front(self):
+#         if self.head == None:
+#             print("List is empty")
+
+#         elif self.head.next == None:
+#             temp = self.head
+
+#             self.head = None
+#             self.tail = None
+#             self.node_num = 0
+
+#             return temp.data
+
+#         else:
+#             temp = self.head
+
+#             temp.next.prev = None
+#             self.head = temp.next
+#             temp.next = None
+#             self.node_num -= 1
+
+#             return temp.data
+
+#     def pop_back(self):
+#         if self.tail == None:
+#             print("List is empty")
+
+#         elif self.tail.prev == None:
+#             temp = self.tail
+
+#             self.head = None
+#             self.tail = None
+#             self.node_num = 0
+
+#             return temp.data
+
+#         else:
+#             temp = self.tail
+
+#             temp.prev.next = None
+#             self.tail = temp.prev
+#             temp.prev = None
+#             self.node_num -= 1
+
+#             return temp.data
+
+#     def size(self):
+#         return self.node_num
+
+#     def empty(self):
+#         return self.node_num == 0
+
+#     def front(self):
+#         if self.head == None:
+#             print("List is empty")
+        
+#         else:
+#             return self.head.data
+
+#     def back(self):
+#         if self.tail == None:
+#             print("List is empty")
+        
+#         else:
+#             return self.tail.data
+
+
+# rep = int(input())
+# dll = DoublyLinkedList()
+
+# for _ in range(rep):
+#     action = input()
+
+#     if action.startswith("push_front"):
+#         num = int(action.split()[1])
+#         dll.push_front(num)
+#     elif action.startswith("push_back"):
+#         num = int(action.split()[1])
+#         dll.push_back(num)
+#     elif action == "pop_front":
+#         print(dll.pop_front())
+#     elif action == "pop_back":
+#         print(dll.pop_back())
+#     elif action == "size":
+#         print(dll.size())
+#     elif action == "empty":
+#         print(1 if dll.empty() else 0)
+#     elif action == "front":
+#         print(dll.front())
+#     elif action == "back":
+#         print(dll.back())
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.node_num = 0
+
+    def push_front(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+
+        #삽입하는 노드가 첫 노드라면
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.prev = None
+
+        #삽입하는 노드가 첫 노드가 아니라면
+        else: 
+            self.head.prev = new_node
+            self.head = new_node
+            new_node.prev = None
+
+        self.node_num += 1
+
+    def push_back(self, new_data):
+        new_node = Node(new_data)
+        new_node.prev = self.tail
+
+        #삽입하는 노드가 첫 노드라면
+        if self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = None
+
+        #삽입하는 노드가 첫 노드가 아니라면
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            new_node.next = None
+
+        self.node_num += 1
+
+    def pop_front(self):
+        if self.head == None:
+            print("List is empty")
+        
+        elif self.head.next == None:
+            temp = self.head
+
+            self.head = None
+            self.tail = None
+            self.node_num = 0
+            return temp.data
+
+        else:
+            temp = self.head
+            
+            temp.next.prev = None
+            self.head = temp.next
+            temp.next = None
+            self.node_num -= 1
+            return temp.data
+
+    def pop_back(self):
+        if self.tail == None:
+            print("List is empty")
+        
+        elif self.tail.prev == None:
+            temp = self.tail
+
+            self.head = None
+            self.tail = None
+            self.node_num = 0
+            return temp.data
+
+        else:
+            temp = self.tail
+
+            temp.prev.next = None
+            self.tail = temp.prev
+            temp.prev = None
+            self.node_num -= 1
+            return temp.data
+
+    def size(self):
+        return self.node_num
+
+    def empty(self):
+        return self.node_num == 0
+
+    def front(self):
+        if self.head == None:
+            print("List is empty")
+        
+        else:
+            return self.head.data
+
+    def back(self):
+        if self.tail == None:
+            print(List is empty)
+
+        else:
+            return self.tail.data
+
+rep = int(input())
+
+dly = DoublyLinkedList()
+
+for _ in range(rep):
+    cmd = input()
+
+    if cmd.startswith("push_front"):
+        _, data = cmd.split()
+        dly.push_front(int(data))
+
+    elif cmd.startswith("push_back"):
+        _, data = cmd.split()
+        dly.push_back(int(data))
+
+    elif cmd.startswith("pop_front"):
+        print(dly.pop_front())
+
+    elif cmd.startswith("pop_back"):
+        print(dly.pop_back())
+
+    elif cmd.startswith("size"):
+        print(dly.size())
+
+    elif cmd.startswith("empty"):
+        print(int(dly.empty()))
+
+    elif cmd.startswith("front"):
+        print(dly.front())
+
+    elif cmd.startswith("back"):
+        print(dly.back())
