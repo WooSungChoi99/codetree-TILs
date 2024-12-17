93442f509342f9fc9ced39e40408d7404e4a4355
WooSungChoi99 <open.w.sung@gmail.com>
2024-12-17 18:15:23 +0900

    Title: 삽입 정렬 구현, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241217/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,27 @@
+# n =  int(input())
+# arr = list(map(int, input().split()))
+
+# for i in range(1, n):
+#     j = i-1
+#     key = arr[i]
+#     while j>=0 and arr[j]>key:
+#         arr[j+1] = arr[j]
+#         j-=1
+#     arr[j+1] = key
+
+# for num in arr:
+#     print(num, end=" ")
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(1, len(arr)):
+    j = i-1
+    key = arr[i]
+    while j>=0 and arr[j]>key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+
+for num in arr:
+    print(num, end=" ")
