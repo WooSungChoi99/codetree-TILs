659e3195ec1a5b326747fafa5047677b4ddc8ef1
WooSungChoi99 <open.w.sung@gmail.com>
2024-12-31 12:37:15 +0900

    Title: 체크판위에서 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241231/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,16 @@
+r, c = tuple(map(int, input().split()))
+arr = [
+    input().split()
+    for _ in range(r)
+]
+
+cnt = 0
+for i in range(1, r-2):
+    for j in range(1, c-2):
+        if arr[i][j]!=arr[0][0]:
+            for k in range(i+1, r-1):
+                for l in range(j+1, c-1):
+                    if arr[k][l]!=arr[i][j]:
+                        cnt += 1
+
+print(cnt)
